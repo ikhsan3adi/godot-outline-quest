4cd75f0281ee8196dbba6449bee610296e6ff71c
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-10-14 11:14:55 +0700

    feat: update AntChallenge to be more variative block
    The AntChallenge now uses a more diverse set of blocks to
    create a more visually interesting and challenging puzzle.

--- Scripts/core/challenges/AntChallenge.cs
+++ Scripts/core/challenges/AntChallenge.cs
@@ -1,45 +1,72 @@
+using Godot;
+
 public class AntChallenge : Challenge
 {
     public AntChallenge()
     {
+        float BASE_LENGTH = PatternBlock.BASE_LENGTH;
+        float TRIANGLE_H = PatternBlock.TRIANGLE_HEIGHT;
+
+        float rad60deg = Mathf.DegToRad(60);
+
         PatternBlocks = [
-            new SquareBlock(new (0, PatternBlock.BASE_LENGTH / 2)),
-            new SquareBlock(new (0, -PatternBlock.BASE_LENGTH / 2)),
-            new HexagonBlock(new (0, - (PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT))),
-            new HexagonBlock(new (0, PatternBlock.BASE_LENGTH * 2), rotationDeg: -30),
+            // kepala
+            new RhombusBlock(new(0, 2.5f * BASE_LENGTH)),
+
+            new TriangleBlock(
+                new(2 * TRIANGLE_H / 3, 2 * BASE_LENGTH),
+                rotationDeg: -30
+            ),
+            new TriangleBlock(
+                new(
+                    Mathf.Cos(rad60deg) * 2 * TRIANGLE_H / 3,
+                    Mathf.Sin(rad60deg) * 2 * TRIANGLE_H / 3 + BASE_LENGTH
+                ),
+                rotationDeg: 30
+            ),
+            new TriangleBlock(
+                new(-2 * TRIANGLE_H / 3, 2 * BASE_LENGTH),
+                rotationDeg: 30
+            ),
+            new TriangleBlock(
+                new(
+                    -Mathf.Cos(rad60deg) * 2 * TRIANGLE_H / 3,
+                    Mathf.Sin(rad60deg) * 2 * TRIANGLE_H / 3 + BASE_LENGTH
+                ),
+                rotationDeg: -30
+            ),
+
+            // badan tengah
+            new SquareBlock(new (0, BASE_LENGTH / 2)),
+            new SquareBlock(new (0, -BASE_LENGTH / 2)),
+
+            // badan bawah
+            new HexagonBlock(new (0, - (BASE_LENGTH + TRIANGLE_H))),
 
             // kaki kanan
             new ThinRhombusBlock(new(
-                    PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT / 2 - PatternBlock.BASE_LENGTH / 20,
-                    PatternBlock.BASE_LENGTH
+                    BASE_LENGTH + TRIANGLE_H / 2 - BASE_LENGTH / 20,
+                    BASE_LENGTH
                 ), rotationDeg: 24
             ),
+            new ThinRhombusBlock(new(BASE_LENGTH + TRIANGLE_H / 2, 0)),
             new ThinRhombusBlock(new(
-                    PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT / 2,
-                    0
-                )
-            ),
-            new ThinRhombusBlock(new(
-                    PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT / 2 - PatternBlock.BASE_LENGTH / 20,
-                    -PatternBlock.BASE_LENGTH
+                    BASE_LENGTH + TRIANGLE_H / 2 - BASE_LENGTH / 20,
+                    -BASE_LENGTH
                 ),
                 rotationDeg: -24
             ),
 
             // kaki kiri
             new ThinRhombusBlock(new(
-                    -(PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT / 2 - PatternBlock.BASE_LENGTH / 20),
-                    PatternBlock.BASE_LENGTH
+                    -(BASE_LENGTH + TRIANGLE_H / 2 - BASE_LENGTH / 20),
+                    BASE_LENGTH
                 ), rotationDeg: -24
             ),
+            new ThinRhombusBlock(new(-(BASE_LENGTH + TRIANGLE_H / 2), 0)),
             new ThinRhombusBlock(new(
-                    -(PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT / 2),
-                    0
-                )
-            ),
-            new ThinRhombusBlock(new(
-                    -(PatternBlock.BASE_LENGTH + PatternBlock.TRIANGLE_HEIGHT / 2 - PatternBlock.BASE_LENGTH / 20),
-                    -PatternBlock.BASE_LENGTH
+                    -(BASE_LENGTH + TRIANGLE_H / 2 - BASE_LENGTH / 20),
+                    -BASE_LENGTH
                 ),
                 rotationDeg: 24
             )
